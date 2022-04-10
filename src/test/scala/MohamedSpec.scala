78f0f2e14a6d5a20a0d7623f864ba97360ecf830
a_szczepanik <a.szczepanik@adform.com>
2022-04-10 18:23:12 +0200

    Setting up test environment

--- src/test/scala/MohamedSpec.scala
+++ src/test/scala/MohamedSpec.scala
@@ -1,16 +1,6 @@
-import org.scalatest._
-import flatspec._
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import Mohamed._
+import org.scalatest.funsuite.AnyFunSuite
 
-class MohamedSpec extends AnyFlatSpec with BeforeAndAfterAll {
+class MohamedSpec extends AnyFunSuite {
 
-  @transient var spark: SparkSession = _
-
-  def loadPlaces(spark: SparkSession, csv: String): DataFrame = spark.read.option("header", value = true).csv(csv)
-
-  "loadPlaces" should "load csv file and return number of counted rows" in {
-    val sampleDF = loadPlaces(spark, "places.csv")
-    val count = sampleDF.count()
-    assert(count == 6, "Record should be 6")
-  }
 }
